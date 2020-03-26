bfa201ea65ca68bcf325b3491ea5a1993fd7375f
Jantzen-E <jantzen.egan@gmail.com>
2020-03-25 22:28:01 -0600

    deployment commit

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,5 @@
-const uriBase = 'http://localhost:5001';
+// const uriBase = 'http://localhost:5001';
+const uriBase = 'https://multiple-to-do-api.herokuapp.com/';
 const api = '/api/v1';
 
 module.exports.uriBase = uriBase
