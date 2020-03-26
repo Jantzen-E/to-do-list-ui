9644344bed8215e124f3c12c0f064341efd5ef07
Jantzen-E <jantzen.egan@gmail.com>
2020-03-25 22:46:52 -0600

    modify heroku url

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 // const uriBase = 'http://localhost:5001';
-const uriBase = 'https://multiple-to-do-api.herokuapp.com/';
+const uriBase = 'https://multiple-to-do-api.herokuapp.com';
 const api = '/api/v1';
 
 module.exports.uriBase = uriBase
